6f7c2ab3c62473461b7968d9027752c25a9a6244
isinnur <isinngnyy@gmail.com>
2024-02-28 00:31:20 +0300

    feat: fetch movies

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,29 @@
-import Image from "next/image";
+import Movies from "@/components/Movies";
+import React from "react";
 
-export default function Home() {
-  return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Veri çekme alanı</h1>
-    </div>
-  );
-}
+const page = async ({ searchParams }) => {
+  const apiKey = "0699b27cb753aebe336f5c310edef70b";
+
+  try {
+    const res = await fetch(
+      `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`
+    );
+    const data = await res.json();
+    console.log(data, "data");
+
+    return (
+      <div>
+        <h1 className="text-3xl font-bold underline">
+          {data?.results?.map((dt, i) => (
+            <Movies key={i} dt={dt} />
+          ))}
+        </h1>
+      </div>
+    );
+  } catch (error) {
+    console.error("Fetch hatası:", error);
+    return null;
+  }
+};
+
+export default page;
---
+++ src/components/Movies.jsx
@@ -0,0 +1,12 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+const Movies = ({ dt }) => {
+  return (
+    <div className="min-w-[400px] h-[300px]">
+      <Image />
+    </div>
+  );
+};
+
+export default Movies;
